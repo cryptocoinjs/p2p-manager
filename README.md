bf2b73b46c2877f2c937c8d52673fd9adeb1fae4
Brooks Boyd <boydb@midnightdesign.ws>
2014-01-03 13:15:37 -0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# P2P Manager
+Manage a network of peers in a peer-to-peer network.
---
+++ examples/bitcoin.js
@@ -0,0 +1,5 @@
+var Manager = require('../lib/Manager').Manager;
+
+var m = new Manager();
+
+m.launch(['dnsseed.bitcoin.dashjr.org']);
